968ab9e86699d9423adecb53bf7fd9241043705f
aminh09cs <18521098@gm.uit.edu.vn>
2025-11-11 00:47:06 +0700

    fix: add /

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import authRoutes from './routes/auth.route.js';
 
 const app = express();
 
-app.use("api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
